42b65e960a97552ece16d03ba4255c819dfaf918
sanjeeb42 <raisanjeeb42@gmail.com>
2024-09-10 23:08:25 +0530

    first commit

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -3,7 +3,7 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
 
 const router = express.Router();
-
+//Adding route
 router.route("/post").post(isAuthenticated, postJob);
 router.route("/get").get(isAuthenticated, getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
