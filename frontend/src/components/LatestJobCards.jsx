c43e544dadc487c8e5e5b114dc4a09e03107d232
sanjeeb42 <raisanjeeb42@gmail.com>
2024-09-12 18:21:41 +0530

    Added LatestJobCards to display Job Openings

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Badge } from "./ui/badge";
 
 const LatestJobCards = () => {
-
   return (
-    <div>
-       LatestJobsCard
-    </div>
-  )
-}
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
+      <div>
+        <h1 className="font-medium text-lg">Company Name</h1>
+        <p className="text-sm text-gray-500">India</p>
+      </div>
 
-export default LatestJobCards
+      <div>
+        <h1 className="font-bold text-lg my-2">Job Title</h1>
+        <p className="text-sm text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime a quo possimus dolores?</p>
+      </div>
+
+      <div className="flex item-center gap-2 mt-4">
+        <Badge className={'text-blue-700 font-bold'} variant="ghost">12 Positions</Badge>
+        <Badge className={'text-[#F83002] font-bold'} variant="ghost">Part Time</Badge>
+        <Badge className={'text-[#7209b7] font-bold'} variant="ghost">24LPA</Badge>
+      </div>
+    </div>
+  );
+};
+
+export default LatestJobCards;
