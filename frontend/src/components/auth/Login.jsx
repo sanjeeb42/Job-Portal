2db73a38dfa9967943939ee852de89cdf6ebc54f
sanjeeb42 <raisanjeeb42@gmail.com>
2024-09-12 12:13:59 +0530

    Added Middleware Mutler. Also Added api calls using axios for Login& Signup

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -4,40 +4,71 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
+import { USER_API_END_POINT } from "@/utils/constant";
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
 
-    const [input, setInput] = useState({
-        email: "",
-        password: "",
-        role: "",
-      });
-    
-      const ChangeEventHandler = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value });
-      };
-      const submitHandler=async (e)=>{
-        e.preventDefault();
-        console.log(input);
-      }
+  const navigate=useNavigate();
+
+  const ChangeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+        try {
+            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true,
+            });
+            console.log(res.data.success);
+            if (res.data.success) {
+                navigate("/");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        } 
+  };
   return (
     <div>
       <Navbar />
       <div className="flex items-center justify-center max-w-7xl mx-auto">
         <form
-           onSubmit={submitHandler}
+          onSubmit={submitHandler}
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
         >
           <h1 className="font-bold text-xl mb-5">Login</h1>
           <div className="my-2">
             <Label>Email</Label>
-            <Input type="email" value={input.email} name="email" onChange={ChangeEventHandler} placeholder="raisanjeeb42@gmail.com" />
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={ChangeEventHandler}
+              placeholder="raisanjeeb42@gmail.com"
+            />
           </div>
-          
+
           <div className="my-2">
             <Label>Password</Label>
-            <Input type="password" value={input.password} name="password" onChange={ChangeEventHandler} placeholder="Password" />
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={ChangeEventHandler}
+              placeholder="Password"
+            />
           </div>
 
           <div className="flex items-center justify-between mx-0">
@@ -50,29 +81,34 @@ const Login = () => {
                   type="radio"
                   name="role"
                   value="student"
-                  checked={input.role==='student'}
+                  checked={input.role === "student"}
                   onChange={ChangeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r1">Student</Label>
               </div>
               <div className="flex items-center space-x-2">
-              <Input
+                <Input
                   type="radio"
                   name="role"
                   value="recruiter"
-                  checked={input.role==='recruiter'}
+                  checked={input.role === "recruiter"}
                   onChange={ChangeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r2">Recruiter</Label>
               </div>
-              
             </RadioGroup>
-            
           </div>
-        <Button variant="destructive" type="submit" className=" w-full my-5">Login</Button>
-        <span className="text-sm">Don't Have an acount? <Link to="/signup" className="text-blue-600">Signup</Link></span>
+          <Button variant="destructive" type="submit" className=" w-full my-5">
+            Login
+          </Button>
+          <span className="text-sm">
+            Don't Have an acount?{" "}
+            <Link to="/signup" className="text-blue-600">
+              Signup
+            </Link>
+          </span>
         </form>
       </div>
     </div>
