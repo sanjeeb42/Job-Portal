af744afa986fb7a064471f2eddc1c510d61a54a3
sanjeeb42 <raisanjeeb42@gmail.com>
2024-09-12 20:38:28 +0530

    Added FilterCard Component to Filter Jobs

--- frontend/src/components/FilterCard.jsx
+++ frontend/src/components/FilterCard.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "@radix-ui/react-label";
 
+const filterData = [
+  {
+    filterType: "Location",
+    array: ["Noida", "Bangalore", "Hyderabad", "Pune", "Mumbai"],
+  },
+  {
+    filterType: "Industry",
+    array: [
+      "Frontend Developer",
+      "Backend Developer",
+      "FullStack Developer",
+      "UI/UX Developer",
+    ],
+  },
+  {
+    filterType: "Salary",
+    array: ["0-40k", "40k-1lakh", "1lakh-5lakh"],
+  },
+];
 const FilterCard = () => {
   return (
-    <div>FilterCard</div>
-  )
-}
+    <div className="w-full bg-white p-3 rounded-md">
+      <h1 className="font-bold text-lg">Filter Jobs</h1>
+      <hr className="mt-3"/>
+      <RadioGroup>
+        {
+            filterData.map((data,index)=>(
+                <div>
+                    <h1 className="font-bold text-lg">{data.filterType}</h1>
+                    {
+                        data.array.map((item,index)=>{
+                            return(
+                                <div className="flex items-center space-x-2 my-2">
+                                    <RadioGroupItem value={item}/>
+                                    <Label>{item}</Label>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            ))
+        }
+      </RadioGroup>
+    </div>
+  );
+};
 
-export default FilterCard
+export default FilterCard;
