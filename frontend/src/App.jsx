ad347db66d9fa6989bf3554d3d39442b33caea81
sanjeeb42 <raisanjeeb42@gmail.com>
2024-09-13 20:18:37 +0530

    Added JobDescription Page Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home'
 import Jobs from './components/Jobs'
 import Browse from './components/Browse'
 import Profile from './components/Profile'
+import JobDescription from './components/JobDescription'
 
 
 const appRouter=createBrowserRouter(
@@ -27,6 +28,10 @@ const appRouter=createBrowserRouter(
       path:'/jobs',
       element:<Jobs/>
     },
+    {
+      path:'/description/:id',
+      element:<JobDescription/>
+    }
     ,
     {
       path:'/browse',
--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -3,8 +3,11 @@ import { Button } from "./ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
 
 const Job = () => {
+  const navigate=useNavigate();
+  const JobId="sdhgsdjk";
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
         <div className="flex items-center justify-between">
@@ -34,7 +37,7 @@ const Job = () => {
         <Badge className={'text-[#7209b7] font-bold'} variant="ghost">24LPA</Badge>
       </div>
       <div className="flex items-center gap-4 mt-4 ">
-        <Button variant="outline" className="rounded-xl">Details</Button>
+        <Button variant="outline" className="rounded-xl" onClick={()=>navigate('/description/${JobId}')}>Details</Button>
         <Button variant="otline" className="bg-[#7209b7] text-[#ffffff] rounded-xl">Save For Later</Button>
       </div>
     </div>
